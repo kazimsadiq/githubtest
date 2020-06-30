631b2c34a7dfe93f3462b60107ec49bda6aaec0a
kazimsadiq <67521069+kazimsadiq@users.noreply.github.com>
2020-06-30 19:28:49 -0400

    Create Working day 2.py

---
+++ Working day 2.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print ("Hello World updated")
+
+def fib(n):
+    a, b =0, 1
+    counter = 1
+    while counter <n:
+        print (a, end=' ')
+        a, b = b, a+b
+        print()
+        counter += 1
+        
+    print(__name__)
+    return(0)
+    
+fib(20)    
